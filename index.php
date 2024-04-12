5a2d290778be403cc4ca488e4b22ea9f28bf47e7
Mega067L <angeljuares067@gmail.com>
2024-04-12 06:39:27 -0600

    colores  upgrade

--- index.php
+++ index.php
@@ -4,11 +4,13 @@
   <meta charset="UTF-8">
   <title>Tienda</title>
   <link rel="stylesheet" type="text/css" href="style.css"> <!-- Enlace al archivo CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/masonry/3.3.4/masonry.min.css">
+
 </head>
 <body>
   <div class="titulo">
     <h1>Tienda de abarrotes </h1>
-    <h1>proyecto </h1>
+    <h1>Angel y Michellel </h1>
   </div>
   <div class="tablas">
     <!-- Incluir el archivo PHP que muestra las tablas -->
