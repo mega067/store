0449787661c9cef8ebcb0d3379386e2062a27ab3
Mega067L <angeljuares067@gmail.com>
2024-04-26 01:00:53 -0600

    se agrego el apartado unidades y el segundo boton

--- c_bebidas.php
+++ c_bebidas.php
@@ -11,6 +11,7 @@ if ($resultado->num_rows > 0) {
         $codigo_de_barras = $fila["codigo_de_barras"];
         $nombre_producto = $fila["nombre_del_producto"];
         $precio_unitario = $fila["precio_unitario"];
+        $unidades = $fila["unidades_producto"];
         $contenido = $fila["contenido"]; // Obtener el contenido del producto
         // Generar la ruta de la imagen (suponiendo que las imágenes están en la misma carpeta)
         $ruta_imagen = "bebidas_img/" . $nombre_producto . ".png";
@@ -22,9 +23,10 @@ if ($resultado->num_rows > 0) {
                 echo '<p>Precio: $' . $precio_unitario . ' MXN</p>';
                 
                 echo '<p class="contenido">' . $contenido . '</p>'; // Mostrar el contenido en un párrafo
+                echo '<p class="contenido">Unidades: '  . $unidades . '</p>'; // Mostrar el contenido en un párrafo
             
-
-            echo '<button class="btn_agregar_carrito" type="button" ><img class="comprar" src="iconos/comprar.png"></button>'; 
+            echo '<button class="btn_rellenar" type="button" ><img class="comprar" src="iconos/comprar.png"></button>'; 
+            echo '<button class="btn_comprar" type="button" ><img class="comprar" src="iconos/comprar.png"></button>'; 
 
         echo '</div>';
        
