40bf8c08796cd0f15f0ff8bce95f90381058809b
Mega067L <angeljuares067@gmail.com>
2024-04-28 19:42:23 -0600

    producto de limpieza

---
+++ prod_limpieza.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Tienda</title>
+  <link rel="stylesheet" type="text/css" href="style.css"> </head>
+
+  <div class="boton">
+  <a href="/store/" class="volver"><img class="volver_img" src="iconos/volver.png"></a>
+
+  <h1 class="titulo">productos de limpieza</h1>
+  
+  </div>
+
+<body>
+
+
+</div>
+ <div class="divcen">
+
+    <!-- Incluir el archivo PHP que muestra las tablas -->
+    <?php include 'c_prod_limpieza.php'; ?>
+ </div>
+ 
+  </div>
+  
+
+</body>
+<footer class="pie">
+  <a href="https://github.com/mega067/store">
+  <img class="git"src="iconos/github.png" alt="git" >
+  <p>github</p>
+  </a>
+    <p>© 2024 Tienda de abarrotes Angel Y Michellel. Todos los derechos reservados.</p>
+  </footer>
+</html>
