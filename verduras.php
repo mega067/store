a626be726761baebffc5b757448aeb27b2fd68c1
Mega067L <angeljuares067@gmail.com>
2024-04-29 20:34:48 -0600

    verduras

---
+++ verduras.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Tienda</title>
+  <link rel="stylesheet" type="text/css" href="style.css"> </head>
+
+  <div class="boton">
+  <a href="/store/" class="volver"><img class="volver_img" src="iconos/volver.png"></a>
+
+  <h1 class="titulo">verduras</h1>
+  
+  </div>
+
+<body>
+<div class="carrito">
+
+
+</div>
+ <div class="divcen">
+
+    <!-- Incluir el archivo PHP que muestra las tablas -->
+    <?php include 'c_verduras.php'; ?>
+ </div>
+ 
+  </div>
+  
+
+</body>
+<footer class="pie">
+  <a href="https://github.com/mega067/store">
+  <img class="git"src="iconos/github.png" alt="git" >
+  <p>github</p>
+  </a>
+    <p>© 2024 Tienda de abarrotes Angel Y Michellel. Todos los derechos reservados.</p>
+  </footer>
+</html>
